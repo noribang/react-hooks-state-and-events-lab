44a0a3a9c8018cd5ea2fc3583ab289b06e6d565e
hoonkim <hoonk9387@gmail.com>
2022-06-05 16:24:48 -0500

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  /* Create state variable.
+     Call useState with initial value.
+     Then array destructure state value and state setter function. */
+  const [inCart, setInCart] = useState(false);
+  /* Event handler.
+     Call setState.*/
+  function handleClick() {
+    console.log(`before click: ${inCart}`)
+    setInCart((inCart) => !inCart);
+    console.log(`after click: ${inCart}`)
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={inCart ? "remove" : "add"} onClick={handleClick}>{inCart ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  // console.log("items: ", items)
+  /* Create state variable.
+     Call useState with initial value.
+     Then array destructure state value and state setter function. */
+    const [selectedCategory, setSelectedCategory] = useState("All");  
+
+  /* Event handler for select option elements. 
+     Changes setState setSelectedCategory(). */
+  function handleFilterChange(event) {
+    console.log(event.target.value);
+    // Update setter function 
+    setSelectedCategory(event.target.value)
+    // console.log(selectedCategory);
+  }
+
+  /* Returns new array of items array of objects to display. */
+  const itemsToDisplay = items.filter((item) => {
+    // If dropdown select value is "All" return 
+    // all objects in array to display.
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +39,10 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {/* {items.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))} */}
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
